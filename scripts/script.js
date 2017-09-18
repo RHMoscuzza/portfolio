4021081519c39a2249c34c3470ba4a2ecac68c23
Beckah Moscuzza <rhmoscuzza@gmail.com>
2017-09-18 08:31:16 -0400

    cleaned up code

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
+console.log("This site designed By Spark-Media https://themeforest.net/item/man-one-page-personal-resumecv-template/20057362?s_rank=5");
+console.log("Photographs by Pam Lau Twitter: @PamLau");
+
 jQuery(document).ready(function() {
-    jQuery('.toggleNav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.menu ul').toggleClass('active');
+    jQuery(".toggleNav").click(function(e) {
+        jQuery(this).toggleClass("active");
+        jQuery(".menu ul").toggleClass("active");
  
         e.preventDefault();
     });
