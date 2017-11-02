ca446949e8bc4e9670e29b09c586f6bc46eaa281
Beckah Moscuzza <rhmoscuzza@gmail.com>
2017-11-02 15:33:06 -0400

    fixed footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-console.log("This site designed By Spark-Media https://themeforest.net/item/man-one-page-personal-resumecv-template/20057362?s_rank=5");
+// console.log("This site designed By Spark-Media https://themeforest.net/item/man-one-page-personal-resumecv-template/20057362?s_rank=5");
 console.log("Photographs by Pam Lau Twitter: @PamLau");
 console.log("Hamburger Menu courtesy https://codepen.io/nickcil/pen/pvPQJK")
 
