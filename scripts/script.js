73d452c9a981f96bc3c251b16ef4e1dfe1d0874c
Beckah Moscuzza <rhmoscuzza@gmail.com>
2017-11-06 21:42:53 -0500

    fixed a js issue that was causing a glitch in toggle menu and cleaned up scss

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
-// console.log("This site designed By Spark-Media https://themeforest.net/item/man-one-page-personal-resumecv-template/20057362?s_rank=5");
 console.log("Photographs by Pam Lau Twitter: @PamLau");
-console.log("Hamburger Menu courtesy https://codepen.io/nickcil/pen/pvPQJK")
 
 const hamFunction = function(){
     const nav = $(".menu ul");
@@ -29,25 +27,15 @@ const hamFunction = function(){
         }
     };
 
-    hamburgerIcon.on("click", function() {
+    hamburgerIcon.on("click", function(e) {
+        e.preventDefault();
         toggleNav();
     });
-
-    // hamburgerIcon.on("mouseenter", function(){
-    //     openNav();
-    // });
 
     navSelection.on("click", function(){
         closeNav();
     });
 
-    // nav.on("mouseleave", function(){
-    //     closeNav();
-    // });
-
-    window.onscroll = function(){
-        closeNav();
-    }
 };
 
 $(function(){
